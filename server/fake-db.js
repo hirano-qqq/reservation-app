13d41a5f99184550a0ca1849a443098a5242f4f2
hirano-qqq <daisangen998@gmail.com>
2024-08-25 09:23:32 +0900

    :wq

---
+++ server/fake-db.js
@@ -0,0 +1,77 @@
+const Product = require('./model/products')
+
+class FakeDb {
+
+    constructor(){
+        this.products = [  {
+            coverImage:'./assets/img/phone-cover.jpg',
+            name: 'Phone XL',
+            price: 799,
+            description: 'A large phone with one of the best screens',
+            heding1: 'サンプルテキスト１',
+            heding2: 'サンプルテキスト2',
+            heding3: 'サンプルテキスト3',
+            hedingtext1: 'サンプルテキスト1サンプルテキストサンプルテキストサンプルテキストサンプルテキスト',
+            hedingtext2: 'サンプルテキスト2サンプルテキストサンプルテキストサンプルテキストサンプルテキスト',
+            hedingtext3: 'サンプルテキスト3サンプルテキストサンプルテキストサンプルテキスト'
+          },{
+            coverImage:"data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==",
+            name: 'Phone Mini',
+            price: 699,
+            description: 'A great phone with one of the best cameras',
+            heding1: 'サンプルテキスト１',
+            heding2: 'サンプルテキスト2',
+            heding3: 'サンプルテキスト3',
+            hedingtext1: 'サンプルテキスト１',
+            hedingtext2: 'サンプルテキスト2',
+            hedingtext3: 'サンプルテキスト3'
+          },
+          {
+            coverImage:'./assets/img/phone-cover.jpg',
+            name: 'Phone Standard',
+            price: 299,
+            description: '',
+            heding1: 'サンプルテキスト１',
+            heding2: 'サンプルテキスト2',
+            heding3: 'サンプルテキスト3',
+            hedingtext1: 'サンプルテキスト１',
+            hedingtext2: 'サンプルテキスト2',
+            hedingtext3: 'サンプルテキスト3'
+          },
+          {
+            coverImage:'./assets/img/phone-cover.jpg',
+            name: 'Phone SSS',
+            price: 999,
+            description: '',
+            heding1: 'サンプルテキスト１',
+            heding2: 'サンプルテキスト2',
+            heding3: 'サンプルテキスト3',
+            hedingtext1: 'サンプルテキスト１',
+            hedingtext2: 'サンプルテキスト2',
+            hedingtext3: 'サンプルテキスト3'
+          }]
+    }
+
+    async initDb(){ 
+        await this.cleanDb()
+        this.pushProductsToDb()    
+    }
+
+    async cleanDb(){
+        await Product.deleteMany({})
+    }
+
+    pushProductsToDb() {
+        this.products.forEach(
+        (product) => {
+            const newProduct = new Product(product)
+            newProduct.save()
+        }
+    )
+    }
+    seeDb(){
+        this.pushProductsToDb()
+    }
+}
+
+module.exports = FakeDb
